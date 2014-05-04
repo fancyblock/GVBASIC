0415ed2a5b1363865522efc13e44149f52b970d3
fancyblock <xujia1985@gmail.com>
2014-05-04 13:32:22 +0800

    some assets added && part of LED

---
+++ GVBASIC/Assets/Script/Display/ASCII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace Assets.Script.Display
+{
+    class ASCII
+    {
+        static public int WIDTH = 8;
+        static public int HEIGHT = 16;
+
+        public int m_asciiCode;
+        public Color[] m_color;
+
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        /// <param name="asciiCode"></param>
+        public ASCII( int asciiCode, Color white, Color black )
+        {
+            m_color = new Color[128];
+
+            //TODO 
+        }
+    }
+}
---
+++ GVBASIC/Assets/Script/Display/LED.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Script.Display;
+
+
+public class LED : MonoBehaviour 
+{
+    public SpriteRenderer m_spriteRender;
+    public Color m_whiteColor;
+
+    protected Texture2D m_texture;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_texture = m_spriteRender.sprite.texture;
+        CleanScreen();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_texture.Apply();
+	}
+
+    /// <summary>
+    /// set pixel 
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="set"></param>
+    public void SetPixel( int x, int y, bool set )
+    {
+        m_texture.SetPixel(x, y, set ? Color.black : m_whiteColor);
+    }
+
+    /// <summary>
+    /// clean screen 
+    /// </summary>
+    public void CleanScreen()
+    {
+        m_texture.SetPixels(0, 0, 160, 80, new Color[] { m_whiteColor });
+        
+        //TODO 
+        ASCII ascii = new ASCII(1, m_whiteColor, Color.black );
+        m_texture.SetPixels(0, 0, ASCII.WIDTH, ASCII.HEIGHT, ascii.m_color);
+    }
+
+}
