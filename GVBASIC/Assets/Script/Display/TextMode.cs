94965789311565085929b48343b46d45feb7201f
fancyblock <xujia1985@gmail.com>
2014-05-05 22:37:54 +0800

    add some new dot matrix

--- GVBASIC/Assets/Script/Display/TextMode.cs
+++ GVBASIC/Assets/Script/Display/TextMode.cs
@@ -26,4 +26,21 @@ public class TextMode : MonoBehaviour
 
         //TODO 
     }
+
+    /// <summary>
+    /// show ASCII 
+    /// </summary>
+    /// <param name="offset"></param>
+    public void ShowASCII( int offset )
+    {
+        for( int i = 0; i < 100; i++ )
+        {
+            int letter = i + offset;
+
+            int x = (i % 20) * 8;
+            int y = i/20 * 16;
+
+            m_led.DrawLetter(x, y, letter, false);
+        }
+    }
 }
