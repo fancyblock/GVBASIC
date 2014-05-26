6e995b12f6d343c603810acbe60376aa41e50ae5
fancyblock <xujia1985@gmail.com>
2014-05-27 00:07:40 +0800

    keyboard stuff

---
+++ GVBASIC/Assets/Script/Keyboard/Keyboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keyboard : MonoBehaviour 
+{
+    public Processor m_processor;
+
+	// Use this for initialization
+	void Start () 
+    {	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if( Input.GetKeyUp( KeyCode.A ) )
+        {
+            m_processor.onKeyUp(KeyEnums.eKeyA);
+        }
+        else if( Input.GetKeyUp( KeyCode.B ))
+        {
+            m_processor.onKeyUp(KeyEnums.eKeyB);
+        }
+        //TODO 
+	}
+
+}
---
+++ GVBASIC/Assets/Script/Processor/Processor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Processor : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	}
+
+    /// <summary>
+    /// on key up 
+    /// </summary>
+    /// <param name="key"></param>
+    public void onKeyUp( KeyEnums key )
+    {
+        //TODO 
+    }
+}
