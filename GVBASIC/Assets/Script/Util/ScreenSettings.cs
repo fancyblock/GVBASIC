83c07df33fb71a7a429b5595aa78863c336f22f8
fancyblock <xujia1985@gmail.com>
2014-05-26 22:51:35 +0800

    add some new dot matrix

---
+++ GVBASIC/Assets/Script/Util/ScreenSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSettings : MonoBehaviour 
+{
+    public int m_screenWidth;
+    public int m_screenHeight;
+    public int m_fps;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Screen.SetResolution(m_screenWidth, m_screenHeight, true, m_fps);
+	}
+	
+}
