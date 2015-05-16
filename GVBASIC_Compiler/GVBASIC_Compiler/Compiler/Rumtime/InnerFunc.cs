98602bfc6cf68dca4ce64a35cd05f83e81bfdc44
fancyblock <xujia1985@gmail.com>
2015-05-17 00:50:46 +0800

    inner math functions (part)

--- GVBASIC_Compiler/GVBASIC_Compiler/Compiler/Rumtime/InnerFunc.cs
+++ GVBASIC_Compiler/GVBASIC_Compiler/Compiler/Rumtime/InnerFunc.cs
@@ -19,10 +19,23 @@ namespace GVBASIC_Compiler.Compiler
             // initial the functions 
             m_funcDic = new Dictionary<string, Func<List<BaseData>, BaseData>>()
             {
+                // math function 
                 {"ABS", ABS},
+                {"SGN", SGN},
+                {"INT", INT},
+                {"SIN", SIN},
+                {"COS", COS},
+                {"TAN", TAN},
+                {"ATN", ATN},
+                {"SQR", SQR},
+                {"EXP", EXP},
+                {"LOG", LOG},
+                {"RND", RND},
+                // string function 
+                {"", ABS},
             };
         }
-
+        
         /// <summary>
         /// set api 
         /// </summary>
@@ -63,8 +76,7 @@ namespace GVBASIC_Compiler.Compiler
         /// <returns></returns>
         protected BaseData ABS( List<BaseData> param )
         {
-            if (param.Count != 1)
-                throw new ErrorCode(ErrorCode.ERROR_CODE_02);
+            checkMathParam(param);
 
             BaseData ret = null;
             BaseData p = param[0];
@@ -73,14 +85,151 @@ namespace GVBASIC_Compiler.Compiler
                 ret = new BaseData(p.m_intVal >= 0 ? p.m_intVal : -p.m_intVal);
             else if (p.m_type == BaseData.TYPE_FLOAT)
                 ret = new BaseData(p.m_floatVal >= 0.0f ? p.m_floatVal : -p.m_floatVal);
-            else
-                throw new ErrorCode(ErrorCode.ERROR_CODE_12);
+
+            return ret;
+        }
+
+        protected BaseData SGN(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData INT( List<BaseData> param )
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData SIN(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData COS(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData TAN(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData ATN(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData SQR(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData EXP(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData LOG(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
+
+            return ret;
+        }
+
+        protected BaseData RND(List<BaseData> param)
+        {
+            checkMathParam(param);
+
+            BaseData ret = null;
+            BaseData p = param[0];
+
+            //TODO 
 
             return ret;
         }
 
 
         #endregion
+
+
+        /// <summary>
+        /// check if math function parameter is correct or not 
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        protected bool checkMathParam( List<BaseData> param )
+        {
+            if (param.Count != 1)
+                throw new ErrorCode(ErrorCode.ERROR_CODE_02);
+
+            BaseData p = param[0];
+
+            if (p.m_type != BaseData.TYPE_INT && p.m_type != BaseData.TYPE_FLOAT)
+                throw new ErrorCode(ErrorCode.ERROR_CODE_12);
+
+            return true;
+        }
 
     }
 }
